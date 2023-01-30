1ef554abbf86761273cf7ad5161900ad60e548c6
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-30 08:31:49 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-import os,platform
+import os
+import sys
+import time
+
+print("\033[1;32mUPDATING ")
 os.system("git pull")
-a=platform.architecture()[0]
-if "64" in a:
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
-    import bkt_enc
-    KAMII.run()
-    elif "32" in a:
-    else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+print("\033[1;32mUPDATE DONE")
+print("\033[1;32mRunning Script ")
+import bkt_enc
