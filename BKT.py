e43337c2689cd5fd1e91bc7493fe4550586e7cdf
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-31 09:27:32 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -2,12 +2,12 @@ import os, platform
 try:
     import requests
 except:
-    os.system('pip install requests')
+    os.system('git pull)
 import requests
 import time
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print('\n\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m');time.sleep(2)
+    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m');time.sleep(2)
     from bkt_enc import asad
     asad()
 elif bit == '32bit':
