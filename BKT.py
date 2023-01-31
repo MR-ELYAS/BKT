a0f7c4503c596768286df0ab7b55ccbd8a990873
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-31 09:26:51 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -11,8 +11,5 @@ if bit == '64bit':
     from bkt_enc import asad
     asad()
 elif bit == '32bit':
-    from bktenc import asad
-    asad()
-else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
     os.system('exit')
