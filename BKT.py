8b7d0987990c54fcf3ab9996d428a02b60c08f6f
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-31 09:26:08 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -4,8 +4,10 @@ try:
 except:
     os.system('pip install requests')
 import requests
+import time
 bit = platform.architecture()[0]
 if bit == '64bit':
+    print('\n\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m');time.sleep(2)
     from bkt_enc import asad
     asad()
 elif bit == '32bit':
