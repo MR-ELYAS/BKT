6285bc9954f01efaff55f88f6e27adda4a0cc84c
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-02-01 17:07:22 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -5,8 +5,6 @@ except:
     os.system('git pull')
 import requests
 import time
-os.system('clear')
-print('BKT TOOL ON UPDATE WAIT FOR UPDATE');exit()
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m');time.sleep(2)
