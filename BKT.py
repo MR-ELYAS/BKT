fc05405b53b33c21c66f9651f870b1311db58f71
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-02-15 10:25:22 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -16,10 +16,9 @@ def Run():
             import H
 
         elif bit == '32bit':
-                        xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
-            xoss("\x1b[1;92m[●] Your Device 32 BIT 💥")
-            xoss('\x1b[1;94m[●] Follow My Facebook First \033[1;97m🎈')
-            os.system('xdg-open https://facebook.com/groups/1367797813762378/')
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
+            xoss("\x1b[1;92m[●] Your Device 32 BIT 💥");time.sleep(1)
+   
             import H
             print(50*"-")
         else:
