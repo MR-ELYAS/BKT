b20433b9fa5fb94d5c778815e9b9ff8d3ed559b9
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-02-15 12:46:34 +0430

    Update BKT.py

--- BKT.py
+++ BKT.py
@@ -15,7 +15,7 @@ def Run():
             xoss("\x1b[1;92m[●] Your Device 64 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] FOLLOW MY GROUP THANKS");time.sleep(1)
             os.system("xdg-open https://facebook.com/groups/1367797813762378/")
-            import H
+            import H64
 
         elif bit == '32bit':
             xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
